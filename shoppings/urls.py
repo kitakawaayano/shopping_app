2cca905eba840b98755cfba4f1f408c36a009003
kitakawaayano <a.kitakawa.sys24@morijyobi.ac.jp>
2025-07-15 14:03:03 +0900

    一覧の表示と画像の追加

--- shoppings/urls.py
+++ shoppings/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import shopping
+from .views import shopping, detail, picture_upload
+from shoppings import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 app_name = "shopping_app"
 urlpatterns = [
-    path('', shopping.as_view(), name='shopping'),
+    path('shop/', shopping, name='shopping'),
+    path('detail/', detail, name='detail'),
+    path('', RedirectView.as_view(url='/shop/')),
+    path('upload/', picture_upload, name='picture_upload'),
+    # path('create/', create, name='create'),
+    
 ]
+
--- shoppings/views.py
+++ shoppings/views.py
@@ -1,9 +1,55 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Shops
+from django.shortcuts import render, redirect
+from django.views import generic, View
+from .models import Shops, Goods, Accounts, Orderhistory
+from .forms import PictureForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
-class shopping(generic.ListView):
-    model = Shops
+# class shopping(generic.ListView):
+#     model = Shops
+#     template_name = 'shopping/index.html'
+    
+# class goods(generic.ListView):
+#     model = Goods
+#     template_name = 'shopping/index.html'
+
+class IndexView(generic.ListView):
+    model = Goods
+    template_name = 'shopping/index.html'
+    def get_context_data(self, **kwargs):
+        shop = super().get_context_data(**kwargs)
+        shop["shops"] = Shops.objects.all
+        return shop
+     
+
+class DetailView(View):
     template_name = 'shopping/index.html'
     
+    def get(self, request, id):
+        picture = Goods.objects.get(goods_id = id)
+        picture = Shops.objects.get(shops_id = id)
+        return render(request, self.template_name, {'picture': picture})
+    
+    
+class PictureUploadView(View):
+    template_name = 'shopping/picture.html'
+    form_class = PictureForm
+    
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('detail')
+        return render(request, self.template_name, {'form': form})
+    
+
+shopping = IndexView.as_view()
+detail = DetailView.as_view()
+picture_upload = PictureUploadView.as_view()
+# create = CreateView.as_view()
+
