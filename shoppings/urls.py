492e49b2bae9ad23dd3755395aaba723448201b0
kitakawaayano <a.kitakawa.sys24@morijyobi.ac.jp>
2025-07-22 10:44:43 +0900

    商品の編集機能を追加

--- shoppings/urls.py
+++ shoppings/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import shopping, detail, picture_upload, create
+from .views import shopping, detail, create, update
 from shoppings import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,8 +10,7 @@ urlpatterns = [
     path('shop/', shopping, name='shopping'),
     path('detail/<int:pk>', detail, name='detail'),
     path('', RedirectView.as_view(url='/shop/')),
-    path('upload/', picture_upload, name='picture_upload'),
     path('create/', create, name='create'),
-    
+    path('update/<int:pk>', update, name='update'),
 ]
 
