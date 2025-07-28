df90aabebb7e1497b256d123df80868d8814aae3
kitakawaayano <a.kitakawa.sys24@morijyobi.ac.jp>
2025-07-28 14:52:19 +0900

    bootstrapの追加とログイン・ログアウト機能

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shoppings',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/shop/'
